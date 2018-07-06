3fac421d987f65f9977ec0ffaed87e7b5ecf7df0
Kaiser-Li <39914654+Kaiser-Li@users.noreply.github.com>
2018-07-06 17:45:50 +0800

    pm

---
+++ src/components/head-menu.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <Menu mode="horizontal" :theme="theme1" active-name="1">
+            <MenuItem name="1">
+                <Icon type="ios-paper"></Icon>
+                内容管理
+            </MenuItem>
+            <MenuItem name="2">
+                <Icon type="ios-people"></Icon>
+                用户管理
+            </MenuItem>
+            <Submenu name="3">
+                <template slot="title">
+                    <Icon type="stats-bars"></Icon>
+                    统计分析
+                </template>
+                <MenuGroup title="使用">
+                    <MenuItem name="3-1">新增和启动</MenuItem>
+                    <MenuItem name="3-2">活跃分析</MenuItem>
+                    <MenuItem name="3-3">时段分析</MenuItem>
+                </MenuGroup>
+                <MenuGroup title="留存">
+                    <MenuItem name="3-4">用户留存</MenuItem>
+                    <MenuItem name="3-5">流失用户</MenuItem>
+                </MenuGroup>
+            </Submenu>
+            <MenuItem name="4">
+                <Icon type="settings"></Icon>
+                综合设置
+            </MenuItem>
+            <MenuItem name="5" style="float:left;margin-left:2rem;border-bottom:1px solid;top:10px;">
+               <img :src="imgurl" class="head-img">
+            </MenuItem>
+        </Menu>
+    </div>
+</template>
+<style scoped>
+    .ivu-menu-horizontal .ivu-menu-item, .ivu-menu-horizontal .ivu-menu-submenu{
+        float:right;
+    }
+    .head-img{
+        width:2.5rem;
+        height:2.5rem;
+    }
+</style>
+<script>
+export default {
+  data () {
+    return {
+      theme1: 'light',
+      imgurl: '../../static/img/logo.png'
+    }
+  }
+}
+</script>
